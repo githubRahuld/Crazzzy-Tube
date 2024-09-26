900903b478ea7edfce747a83a87a4343a58be7ec
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-09-26 15:56:07 +0530

    Update Navbar.jsx

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -77,27 +77,98 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
 
   return (
     <>
-      {isLoggedIn ? (
-        <nav className="bg-white border-b border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col sm:flex-row sm:items-center justify-between h-auto">
-              {/* First Row: Logo, Search Bar, and Profile Avatar */}
-              <div className="w-full flex items-center justify-between h-16">
-                {/* Logo */}
-                <div className="flex-shrink-0">
-                  <Link to="/users/home">
-                    <img
-                      className="h-28 w-28"
-                      src="/img/Logo.png"
-                      alt="YouTube Logo"
-                    />
-                  </Link>
+      {location.pathname !== "/users/login" &&
+        (isLoggedIn ? (
+          <nav className="bg-white border-b border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex flex-col sm:flex-row sm:items-center justify-between h-auto">
+                {/* First Row: Logo, Search Bar, and Profile Avatar */}
+                <div className="w-full flex items-center justify-between h-16">
+                  {/* Logo */}
+                  <div className="flex-shrink-0">
+                    <Link to="/users/home">
+                      <img
+                        className="h-28 w-28"
+                        src="/img/Logo.png"
+                        alt="YouTube Logo"
+                      />
+                    </Link>
+                  </div>
+
+                  {/* Search Bar for larger screens */}
+                  {location.pathname === "/users/home" && (
+                    <div className="hidden sm:flex sm:flex-grow sm:ml-4">
+                      <div className="relative w-full">
+                        <input
+                          type="text"
+                          className="w-full py-2 px-4 border border-gray-300 rounded-full dark:border-gray-600 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                          placeholder="Search"
+                          value={searchTerm}
+                          onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <button className="absolute right-0 top-0 mt-2 mr-3 text-gray-500 dark:text-gray-400">
+                          <FaSearch className="w-6 h-6" />
+                        </button>
+                      </div>
+                    </div>
+                  )}
+
+                  {/* Upload Button and Profile Avatar */}
+                  <div className="flex items-center space-x-2">
+                    <Tooltip content="Post a video">
+                      <Button className="bg-white" onClick={handleUpload}>
+                        <RiVideoUploadFill
+                          size={30}
+                          className="mr-2 cursor-pointer"
+                        />
+                      </Button>
+                    </Tooltip>
+                    <Dropdown placement="bottom-end">
+                      <DropdownTrigger>
+                        <Avatar
+                          isBordered
+                          as="button"
+                          className="transition-transform"
+                          src={avatar}
+                        />
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Profile Actions" variant="flat">
+                        <DropdownItem
+                          key="profile"
+                          className="h-14 gap-2"
+                          textValue="Signed in as zoey@example.com"
+                        >
+                          <p className="font-semibold">Signed in as</p>
+                          <p className="font-semibold">{email}</p>
+                        </DropdownItem>
+                        <DropdownItem key="settings" textValue="Profile">
+                          <Link to="/users/profile">Profile</Link>
+                        </DropdownItem>
+                        <DropdownItem key="team_settings" textValue="Dashboard">
+                          <Link to={`/users/dashboard/${username}`}>
+                            Dashboard
+                          </Link>
+                        </DropdownItem>
+                        <DropdownItem key="tweet" textValue="Tweet">
+                          <Link to={`/tweet`}>Tweet</Link>
+                        </DropdownItem>
+                        <DropdownItem
+                          key="logout"
+                          color="danger"
+                          textValue="Log Out"
+                          onClick={handleLogout}
+                        >
+                          Log Out
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
                 </div>
 
-                {/* Search Bar for larger screens */}
+                {/* Search Bar for small screens */}
                 {location.pathname === "/users/home" && (
-                  <div className="hidden sm:flex sm:flex-grow sm:ml-4">
-                    <div className="relative w-full">
+                  <div className="w-full sm:hidden mt-2">
+                    <div className="relative w-full mb-2">
                       <input
                         type="text"
                         className="w-full py-2 px-4 border border-gray-300 rounded-full dark:border-gray-600 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -111,80 +182,10 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
                     </div>
                   </div>
                 )}
-
-                {/* Upload Button and Profile Avatar */}
-                <div className="flex items-center space-x-2">
-                  <Tooltip content="Post a video">
-                    <Button className="bg-white" onClick={handleUpload}>
-                      <RiVideoUploadFill
-                        size={30}
-                        className="mr-2 cursor-pointer"
-                      />
-                    </Button>
-                  </Tooltip>
-                  <Dropdown placement="bottom-end">
-                    <DropdownTrigger>
-                      <Avatar
-                        isBordered
-                        as="button"
-                        className="transition-transform"
-                        src={avatar}
-                      />
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Profile Actions" variant="flat">
-                      <DropdownItem
-                        key="profile"
-                        className="h-14 gap-2"
-                        textValue="Signed in as zoey@example.com"
-                      >
-                        <p className="font-semibold">Signed in as</p>
-                        <p className="font-semibold">{email}</p>
-                      </DropdownItem>
-                      <DropdownItem key="settings" textValue="Profile">
-                        <Link to="/users/profile">Profile</Link>
-                      </DropdownItem>
-                      <DropdownItem key="team_settings" textValue="Dashboard">
-                        <Link to={`/users/dashboard/${username}`}>
-                          Dashboard
-                        </Link>
-                      </DropdownItem>
-                      <DropdownItem key="team_settings" textValue="Dashboard">
-                        <Link to={`/tweet`}>Tweet</Link>
-                      </DropdownItem>
-                      <DropdownItem
-                        key="logout"
-                        color="danger"
-                        textValue="Log Out"
-                        onClick={handleLogout}
-                      >
-                        Log Out
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                </div>
               </div>
-
-              {/* Search Bar for small screens */}
-              {location.pathname === "/users/home" && (
-                <div className="w-full sm:hidden mt-2">
-                  <div className="relative w-full mb-2">
-                    <input
-                      type="text"
-                      className="w-full py-2 px-4 border border-gray-300 rounded-full dark:border-gray-600 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                      placeholder="Search"
-                      value={searchTerm}
-                      onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <button className="absolute right-0 top-0 mt-2 mr-3 text-gray-500 dark:text-gray-400">
-                      <FaSearch className="w-6 h-6" />
-                    </button>
-                  </div>
-                </div>
-              )}
             </div>
-          </div>
-        </nav>
-      ) : null}
+          </nav>
+        ) : null)}
     </>
   );
 };
