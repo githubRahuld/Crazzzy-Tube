6d7c92351d37ea27ae4d6f355de30a55c92a2a06
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-09-27 23:34:59 +0530

    Update multer.middleware.js

--- Backend/src/middleware/multer.middleware.js
+++ Backend/src/middleware/multer.middleware.js
@@ -2,16 +2,16 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
-// Configure Cloudinary storage for multer
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'temp', // Folder in Cloudinary where files will be uploaded
-        public_id: (req, file) => uuidv4(), // Unique filename
-        format: async (req, file) => path.extname(file.originalname).replace('.', ''), // Get the file extension
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + uuidv4() + path.extname(file.originalname)
+        );
     },
 });
 
-// Multer setup to use Cloudinary storage
 export const upload = multer({ storage: storage });
-
