e1ec54569028fe3cd9ac1084d1cb180d8b8ce16b
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-09-28 11:24:54 +0530

    Update multer.middleware.js

--- Backend/src/middleware/multer.middleware.js
+++ Backend/src/middleware/multer.middleware.js
@@ -1,17 +1,5 @@
 import multer from "multer";
-import { v4 as uuidv4 } from "uuid";
-import path from "path";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + uuidv4() + path.extname(file.originalname)
-        );
-    },
-});
+const storage = multer.diskStorage({});
 
 export const upload = multer({ storage: storage });
