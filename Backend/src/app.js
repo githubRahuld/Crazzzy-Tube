dd5802a85e8d0fb62df277b75feb5251467aeed6
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2024-10-02 22:35:40 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,10 +19,11 @@ app.options("*", cors(corsOptions));
 
 
 // allowing json data
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: '50mb' })); // Adjust as needed
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //for url consistency
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //allowing public folder to access
 app.use(express.static("public"));
