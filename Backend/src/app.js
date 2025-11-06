f9d77c55a356f8830e87569d53c0f34e2bbf0412
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2025-11-06 14:09:29 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,9 +14,6 @@ app.use(
 );
 
 
-// Ensure preflight (OPTIONS) requests also use the same CORS settings
-app.options("*", cors(corsOptions));
-
 
 // allowing json data
 app.use(express.json({ limit: '50mb' })); // Adjust as needed
