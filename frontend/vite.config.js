0df5f8d4591699001da4e2fa21ffd74e52c159fa
Rahul Dhakad <86297971+githubRahuld@users.noreply.github.com>
2025-11-06 14:14:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-       "/api": {
-        target: "https://crazzzy-tube-backend.vercel.app", 
-        changeOrigin: true,
-        secure: false,
-        configure: (proxy, options) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["Access-Control-Allow-Origin"] = "https://crazzzy-tube-frontend.vercel.app";
-            proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
-            proxyRes.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
-            proxyRes.headers["Access-Control-Allow-Headers"] = "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version";
-          });
-        },
-       }
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
